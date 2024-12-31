421e2e11e476fbcf7f1687623ce7a8d28bcc5066
Irene Spanou <2804507+iresil@users.noreply.github.com>
2024-12-31 15:11:25 +0200

    Fix destruction staff visual bug on inactive ability bar when using FAB+
    + Attempt to algorithmically identify whether an icon should be replaced or not.
    
    Co-Authored-By: Jason P. <586496+Vartalus@users.noreply.github.com>

--- InactiveActionBarFAB.lua
+++ InactiveActionBarFAB.lua
@@ -11,13 +11,35 @@ function CustomAbilityIcons.ApplySkillStyleInactiveFAB(slotIndex, hotbarCategory
     if FAB then
         local icon = CustomAbilityIcons.GetSkillStyleIcon(slotIndex, hotbarCategory)
                      or CustomAbilityIcons.GetCustomAbilityIcon(slotIndex, hotbarCategory)
-                     or CustomAbilityIcons.GetDefaultAbilityIcon(slotIndex, hotbarCategory)
         if (icon or "") ~= "" then
             slotIndex = slotIndex + CustomAbilityIcons.SLOT_INDEX_OFFSET
             --- @diagnostic disable-next-line: param-type-mismatch
             CustomAbilityIcons.ReplaceAbilityBarIcon(slotIndex, hotbarCategory, icon)
         end
     end
+end
+
+--- Decides whether icon replacement should take place or not. The only reason not to perform it, is if we've been asked to draw
+--- the icons of the inactive action bar and the inactive weapon is a destruction staff.
+--- @param hotbarCategory number The category of the hotbar in question.
+--- @return boolean skipIconReplacement True if CustomAbilityIcons should not perform icon replacement. False otherwise.
+function CustomAbilityIcons.SkipSkillIconReplacement(hotbarCategory)
+    local result = false
+    local activeHotbarCategory = GetActiveHotbarCategory()
+    local inactiveWeapon = nil
+    if activeHotbarCategory ~= hotbarCategory then
+        if hotbarCategory == HOTBAR_CATEGORY_BACKUP then
+            inactiveWeapon = GetItemLinkWeaponType(GetItemLink(BAG_WORN, EQUIP_SLOT_BACKUP_MAIN, LINK_STYLE_DEFAULT))
+        else
+            inactiveWeapon = GetItemLinkWeaponType(GetItemLink(BAG_WORN, EQUIP_SLOT_MAIN_HAND, LINK_STYLE_DEFAULT))
+        end
+    end
+    if inactiveWeapon == WEAPONTYPE_FIRE_STAFF
+       or inactiveWeapon == WEAPONTYPE_FROST_STAFF
+       or inactiveWeapon == WEAPONTYPE_LIGHTNING_STAFF then
+        result = true
+    end
+    return result
 end
 
 --- Retrieves the inactive action bar button corresponding to the specified slotIndex, provided that
